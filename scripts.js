8d3e38f5bbce393a57ead18f15b7d870a8ac44f6
Jason <jasonries1441@gmail.com>
2023-05-21 06:58:42 +0200

    5

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// For responsive nav Menu
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -6,7 +7,8 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 })
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
